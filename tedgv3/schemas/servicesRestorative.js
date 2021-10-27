4aad46f175fe8cc0038ce5d2a89526f1e666da43
Omar Ong <marionong@gmail.com>
2021-10-27 19:15:40 -0400

    restorative dental services schema
    Signed-off-by: Omar Ong <marionong@gmail.com>

---
+++ tedgv3/schemas/servicesRestorative.js
@@ -0,0 +1,31 @@
+export default {
+    name: "servicesRestorative",
+    title: "Services -- RESTORATIVE",
+    type: "document",
+    fields: [
+        {
+            name: "serviceName",
+            title: "Name of Service",
+            type: "string",
+        },
+        {
+            name: "serviceDescription",
+            title: "Service Description",
+            type: "array",
+            of: [
+                {
+                    type: 'block'
+                }
+            ]
+        },
+        {
+            name: 'serviceImage',
+            title: "Service Image",
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+    ],
+
+}
