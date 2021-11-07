87cd1f18955972964fb21e8702b043b600c77076
Omar Ong <marionong@gmail.com>
2021-11-07 14:05:29 -0500

    navbar rename and refactor to include dropdown selections

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar, Nav, Container, Button} from "react-bootstrap";
+import {Navbar, Nav, Container, Button, NavDropdown} from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import styled from 'styled-components';
 
@@ -12,7 +12,7 @@ const MenuDiv = styled(Nav)`
     }
 `
 
-const MenuBar = () => {
+const NavBar = () => {
     return (
 
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
@@ -55,13 +55,13 @@ const MenuBar = () => {
                         </LinkContainer>
 
 
-                        {/*<NavDropdown title="Dropdown" id="collasible-nav-dropdown">*/}
-                        {/*    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
-                        {/*    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>*/}
-                        {/*    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>*/}
-                        {/*    <NavDropdown.Divider />*/}
-                        {/*    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>*/}
-                        {/*</NavDropdown>*/}
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
                     </MenuDiv>
                     <MenuDiv sideMargins="10px">
                         <LinkContainer to="#">
@@ -87,33 +87,33 @@ const MenuBar = () => {
     );
 }
 
-export default MenuBar;
+export default NavBar;
 
 
-
-<Container>
-    <header className='d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom'>
-        <LinkContainer to='/smile-gallery'>
-            <Nav.Link className='d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none'>
-                Smile Gallery
-            </Nav.Link>
-        </LinkContainer>
-        <Nav>
-            <ul className='nav col-12 col-md-auto mb-2 justify-content-center mb-md-0'>
-                <li>
-                    <LinkContainer to='/about'>
-                        <Nav.Link className='nav-link px-2 link-secondary'>About</Nav.Link>
-                    </LinkContainer>
-                </li>
-            </ul>
-
-            <div className='col-md-3 text-end'>
-                <LinkContainer to='/'>
-                    <button type='button' className='btn btn-outline-primary me-2'>
-                        Home
-                    </button>
-                </LinkContainer>
-            </div>
-        </Nav>
-    </header>
-</Container>
+//
+// <Container>
+//     <header className='d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom'>
+//         <LinkContainer to='/smile-gallery'>
+//             <Nav.Link className='d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none'>
+//                 Smile Gallery
+//             </Nav.Link>
+//         </LinkContainer>
+//         <Nav>
+//             <ul className='nav col-12 col-md-auto mb-2 justify-content-center mb-md-0'>
+//                 <li>
+//                     <LinkContainer to='/about'>
+//                         <Nav.Link className='nav-link px-2 link-secondary'>About</Nav.Link>
+//                     </LinkContainer>
+//                 </li>
+//             </ul>
+//
+//             <div className='col-md-3 text-end'>
+//                 <LinkContainer to='/'>
+//                     <button type='button' className='btn btn-outline-primary me-2'>
+//                         Home
+//                     </button>
+//                 </LinkContainer>
+//             </div>
+//         </Nav>
+//     </header>
+// </Container>
