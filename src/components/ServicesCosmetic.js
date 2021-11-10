2c3d12518c165a627ec6489468572144985bcf43
Omar Ong <marionong@gmail.com>
2021-11-10 18:22:28 -0500

    api led cosmetic dentistry page

--- src/components/ServicesCosmetic.js
+++ src/components/ServicesCosmetic.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Breadcrumb, BreadcrumbItem, Button, Col, Container, Image, Row, Stack} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
 import TestimonialSection from "./TestimonialSection";
 import PreFooter from "./PreFooter";
 import Footer from "./Footer";
 import cardSample from "../assets/images/card-sample2x.png";
+import sanityClient from "../client";
+import PortableText from "@sanity/block-content-to-react";
+import styled from "styled-components";
 
 const ButtonThing = (props) => (
     <div className="text-left">
@@ -12,18 +15,56 @@ const ButtonThing = (props) => (
             <Button variant="outline-primary" size="md">{props.copy}</Button>
         </LinkContainer>
     </div>
-
 )
+
+const StyledImage = styled(Image)`
+  //max-width: 600px;
+  //max-height: 468px;
+
+  @media screen and (max-width: 480px) {
+
+  }
+`
 
 
 function ServicesCosmetic(props) {
 
-    let isSubhead = true;
     let isPic = true;
     let isSlug = true;
 
 
     let slugName = '/about-us/meet-the-team'
+
+    const [serviceInfo, setServiceInfo] = useState(null);
+
+    const [serviceOfferings, setServiceOfferings] = useState(null);
+
+    useEffect(() => {
+        // grok custom sanity query lang (similar to graphQL)
+        sanityClient.fetch(`*[_type == "servicesList"] {
+            description
+        }`)
+            .then((data) => setServiceInfo(data))
+            .catch(console.error);
+    }, []);
+
+    useEffect(() => {
+        // grok custom sanity query lang (similar to graphQL)
+        sanityClient.fetch(`*[_type == "servicesCosmetic"] {
+            serviceName,
+            serviceDescription,
+            serviceImage{
+                asset->{
+                    _id,
+                    url
+                },
+                alt
+            }
+        }`)
+            .then((data) => setServiceOfferings(data))
+            .catch(console.error);
+    }, []);
+
 
     return (
         <Container>
@@ -50,30 +91,35 @@ function ServicesCosmetic(props) {
 
                 <h1 className="text-center mt-4 mb-4 pb-4">Cosmetic Dentistry</h1>
 
-                {isSubhead &&
-                    <h2 className="text-center mt-4 mb-4">Conveniently located in Manhattan’s Financial District, our office
-                    offers a variety of teeth-straightening options with our in-house
-                    orthodontist, Dr. Weeman.
-                    </h2>
+                {serviceInfo &&
+                <h2 className="text-center mt-4 mb-4">
+                    <PortableText blocks={serviceInfo[0].description}/>
+                </h2>
                 }
 
+                {serviceOfferings && serviceOfferings.map((content, index) => {
+                    console.log("service content: ", content);
 
-                <Row className="mt-5">
-                    {isPic
+                    return (
+                        <span key={index}>
+                            <Row className="mt-5 justify-content-between">
+                    {content.serviceImage
                         ? <>
-                            <Col sm={12} md={6}>
-                                <Image
-                                    src={cardSample}
+                            <Col sm={12} md={5} className="px-3">
+                                <StyledImage
+                                    src={content.serviceImage.asset.url}
                                     alt=""
                                     className=""
                                     fluid
                                 />
 
                             </Col>
-                            <Col sm={12} md={6}>
+                            <Col sm={12} md={7} className="px-4 pt-md-0 pt-3">
                                 <Stack gap={2}>
-                                    <h2>MEET THE TEAM</h2>
-                                    <p>You’re in great hands. Our dedicated team of dentists, specialists, and hygienists have decades of experience and form the fabric of our practice. </p>
+                                    <h2>{content.serviceName}</h2>
+                                    <p>
+                                        <PortableText blocks={content.serviceDescription}/>
+                                    </p>
                                     {
                                         isSlug &&
                                         <>
@@ -88,8 +134,10 @@ function ServicesCosmetic(props) {
                         : <>
                             <Col sm={12} md={12}>
                                 <Stack gap={2}>
-                                    <h2>MEET THE TEAM</h2>
-                                    <p>You’re in great hands. Our dedicated team of dentists, specialists, and hygienists have decades of experience and form the fabric of our practice. </p>
+                                    <h2>{content.serviceName}</h2>
+                                    <p>
+                                        <PortableText blocks={content.serviceDescription}/>
+                                    </p>
                                     {
                                         isSlug &&
                                         <>
@@ -102,6 +150,13 @@ function ServicesCosmetic(props) {
                     }
 
                 </Row>
+                        </span>
+                    )
+
+                    })
+
+                }
+
             <TestimonialSection/>
             <PreFooter/>
             <Footer/>
