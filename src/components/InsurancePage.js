e3751bd34457856b1e691d8b7042fef9e8e609b4
Omar Ong <marionong@gmail.com>
2021-11-14 22:35:14 -0500

    insurance page wip

--- src/components/InsurancePage.js
+++ src/components/InsurancePage.js
@@ -7,15 +7,48 @@ import styled from "styled-components";
 import TestimonialSection from "./TestimonialSection";
 import PreFooter from "./PreFooter";
 import Footer from "./Footer";
-import cardSample from "../assets/images/card-sample2x.png";
-import covidImg from '../assets/images/pt-info-covid.png'
-import insuranceImg from '../assets/images/pt-info-insurance.png'
-import smileImg from "../assets/images/home-smile-img.jpg";
+import insurance1 from "../assets/images/insurance 1.png";
+import insurance2 from "../assets/images/insurance 2.jpg";
+import insurance3 from "../assets/images/insurance 3.png";
+import insurance4 from "../assets/images/insurance 4.png";
+import insurance5 from "../assets/images/insurance 5.png";
+import insurance6 from "../assets/images/insurance 6.png";
+import insurance7 from "../assets/images/insurance 7.jpg";
+import card1 from "../assets/images/card1.png";
+import card2 from "../assets/images/card 2.png";
+import card3 from "../assets/images/card3.png";
+import card4 from "../assets/images/card 4.png";
+import card0 from "../assets/images/card 0.png";
+import payment1 from "../assets/images/payment 1.png";
+import payment2 from "../assets/images/payment 2.png";
+import careCreditLogo from '../assets/images/carecreditlogo.png';
+
+
+const CareCreditImg = styled(Image)`
+    width: 330px;
+    margin-left: 60px;
+      @media (max-width: 768px) {
+        width: 250px;
+        margin: 10px 40px;
+      }
+`
 
 const Div85WidthCentered = styled.div`
   width: 85%;
   margin: 0 auto;
   align-items: center;
+`
+
+const IconRowImage = styled(Image)`
+  height: 50px;
+`
+
+const StyledH1 = styled.h1`
+  font-size: 3rem;
+  @media (max-width: 768px) {
+    font-size: 2rem;
+    margin-top: 2rem;
+  }
 `
 
 function InsurancePage(props) {
@@ -45,23 +78,24 @@ function InsurancePage(props) {
     return (
         <>
             <Container fluid>
+                <Stack gap={5}>
                 {patientInfoContent && patientInfoContent.map((content,index) => {
                     console.log("CONTENT IS: ", content);
                     return (
                         <span key={index}>
-                            <Stack gap={5}>
-
-                                <div>
-                                    <Image
-                                        src={content.bannerImage.asset.url}
-                                        alt=""
-                                        className=""
-                                        fluid
-                                    />
-                                </div>
 
 
-                                <Breadcrumb>
+                                {/*<div>*/}
+                                {/*    <Image*/}
+                                {/*        src={content.bannerImage.asset.url}*/}
+                                {/*        alt=""*/}
+                                {/*        className=""*/}
+                                {/*        fluid*/}
+                                {/*    />*/}
+                                {/*</div>*/}
+
+
+                                <Breadcrumb className="mt-4">
                                     <LinkContainer to="/">
                                         <BreadcrumbItem>
                                           <a href="home">
@@ -69,6 +103,13 @@ function InsurancePage(props) {
                                           </a>
                                         </BreadcrumbItem>
                                     </LinkContainer>
+                                                                    <LinkContainer to="/patient-info">
+                                    <BreadcrumbItem>
+                                      <a href="home">
+                                        Patient Information
+                                      </a>
+                                    </BreadcrumbItem>
+                                </LinkContainer>
                                     <BreadcrumbItem active>
                                         Insurance
                                         {/*{content.pageName}*/}
@@ -76,21 +117,196 @@ function InsurancePage(props) {
                                 </Breadcrumb>
 
 
-                                <div>
-                                    <h1 className="text-center">
-                                        Accepted Insurance
-                                        {/*{content.headliner}*/}
-                                    </h1>
-                                </div>
-                                <Div85WidthCentered>
-                                    <h3 className="text-center">
-One of the many aspects of our practice that has endeared us to our patients over the years is that while we are a modern, beautiful, and inviting dental practice, we accept a multitude of PPO insurance plans.                                        {/*<PortableText blocks={content.subHeadline}/>*/}
-                                    </h3>
-                                </Div85WidthCentered>
+                            <div>
+                                <StyledH1 className="text-left mt-md-5">
+                                    Accepted Insurance
+                                    {/*{content.headliner}*/}
+                                </StyledH1>
+                            </div>
+
+                            <div className="text-left mt-5">
+                                <h3>
+One of the many aspects of our practice that has endeared us to our patients over the years is that while we are a modern, beautiful, and inviting dental practice, we accept a multitude of PPO insurance plans.
+                                </h3>
+                            </div>
+
+                            <Row className="mt-md-1">
+                                <Col md={12} className="mt-md-5">
+
+                                    <div className="d-flex flex-md-nowrap flex-wrap align-content-center align-items-center justify-content-around mt-2 mb-5">
+                                        <IconRowImage
+                                            alt=""
+                                            className="insurance"
+                                            src={insurance1}
+                                            fluid
+                                        />
+                                        <IconRowImage
+                                            alt=""
+                                            className="insurance"
+                                            src={insurance2}
+                                            fluid
+
+                                        />
+                                        <IconRowImage
+                                            alt=""
+                                            className="insurance"
+                                            src={insurance3}
+                                            fluid
+                                        />
+                                        <IconRowImage
+                                            alt=""
+                                            className="insurance"
+                                            src={insurance4}
+                                            fluid
+                                        />
+                                        <IconRowImage
+                                            alt=""
+                                            className="insurance"
+                                            src={insurance5}
+                                            fluid
+                                        />
+                                        <IconRowImage
+                                            alt=""
+                                            className="insurance"
+                                            src={insurance6}
+                                            fluid
+                                        />
+                                        <IconRowImage
+                                            alt=""
+                                            className="insurance"
+                                            src={insurance7}
+                                            fluid
+                                        />
+
+                                    </div>
+                                </Col>
+                                <Col md={6}>
+                                    <h4>
+                                       <ul>
+                                            <li>Aetna</li>
+                                            <li>Blue Cross Blue Shield</li>
+                                            <li>Careington</li>
+                                            <li>Cigna</li>
+                                            <li>Delta</li>
+                                            <li>Dental Health Alliance</li>
+                                            <li>First Fortis</li>
+                                            <li>First Reliance</li>
+                                            <li>Guardian</li>
+                                            <li>Humana</li>
+                                        </ul>
+                                    </h4>
+
+                                </Col>
+                                <Col md={6}>
+                                    <h4>
+                                        <ul>
+                                            <li>Lincoln</li>
+                                            <li>MetLife</li>
+                                            <li>Nippon</li>
+                                            <li>Principal</li>
+                                            <li>Renaissance</li>
+                                            <li>Sunlife Financial</li>
+                                            <li>Unicare</li>
+                                            <li>United Concordia</li>
+                                            <li>United Healthcare</li>
+                                            <li>& More</li>
+                                        </ul>
+                                    </h4>
+
+                                </Col>
+                                <Col md={12} className="mt-md-5">
+                                    <strong>
+                                        <h4>Please note that we are not affiliated with any HMO or DMO plans.</h4>
+                                    </strong>
+                                </Col>
+
+                            </Row>
+
+                            <div>
+                                <StyledH1 className="text-left mt-md-5 pt-md-5">
+                                    Payment Options
+                                </StyledH1>
+                            </div>
+                            <Row>
+                                <Col md={12} className="mt-md-5">
+
+                                    <div className="d-flex flex-md-nowrap flex-wrap align-content-center align-items-center justify-content-around mt-2 mb-5">
+                                        <Card border="light" style={{ width: '10rem' }} className="text-center">
+                                          <Card.Img variant="top" src={payment1} />
+                                          <Card.Body>
+                                            <Card.Title>Cash</Card.Title>
+                                          </Card.Body>
+                                        </Card>
+                                        <Card border="light" style={{ width: '10rem' }} className="text-center">
+                                          <Card.Img variant="top" src={payment2} />
+                                          <Card.Body>
+                                            <Card.Title>Check</Card.Title>
+                                          </Card.Body>
+                                        </Card>
+                                        <Card border="light" style={{ width: '10rem' }} className="text-center">
+                                          <Card.Img variant="top" src={card0} />
+                                          <Card.Body>
+                                            <Card.Title>Debit</Card.Title>
+                                          </Card.Body>
+                                        </Card>
+                                        <Card border="light" style={{ width: '10rem' }} className="text-center">
+                                          <Card.Img variant="top" src={card1} />
+                                          <Card.Body>
+                                            <Card.Title>Visa</Card.Title>
+                                          </Card.Body>
+                                        </Card>
+                                        <Card border="light" style={{ width: '10rem' }} className="text-center">
+                                          <Card.Img variant="top" src={card2} />
+                                          <Card.Body>
+                                            <Card.Title>Discover</Card.Title>
+                                          </Card.Body>
+                                        </Card>
+                                        <Card border="light" style={{ width: '10rem'}} className="text-center">
+                                          <Card.Img variant="top" src={card3} />
+                                          <Card.Body>
+                                            <Card.Title>MasterCard</Card.Title>
+                                          </Card.Body>
+                                        </Card>
+                                        <Card border="light" style={{ width: '10rem' }} className="text-center">
+                                          <Card.Img variant="top" src={card4} />
+                                          <Card.Body>
+                                            <Card.Title>Amex</Card.Title>
+                                          </Card.Body>
+                                        </Card>
+                                    </div>
+                                </Col>
+                            </Row>
+
+
+                            <div>
+                                <StyledH1 className="text-left mt-md-5">
+                                    Financing Options
+                                </StyledH1>
+                            </div>
+
+                            <Row className="text-left mt-5">
+                                <Col md={6}>
+                                    <div className="text-start text-">
+                                        <h3>
+        <em>CareCredit</em> offers special financing and low monthly payment options, no annual fees, setup costs, or prepayment penalties. Whether you have dental insurance or not, many services aren’t covered. CareCredit can help you get the healthy, radiant smile you’ve always wanted with low monthly dental payment plans.
+                                        </h3>
+                                    </div>
+
+                                </Col>
+                                <Col md={6} className="px-md-5">
+                                    <CareCreditImg
+                                        src={careCreditLogo}
+                                        alt="care credit logo"
+                                        // className="px-md-5"
+
+                                    />
+                                </Col>
+
+                            </Row>
 
 
 
-                            </Stack>
+
                         </span>
                     )
 
@@ -102,7 +318,7 @@ One of the many aspects of our practice that has endeared us to our patients ove
                 <PreFooter/>
                 <Footer/>
 
-
+                </Stack>
             </Container>
         </>
     );
