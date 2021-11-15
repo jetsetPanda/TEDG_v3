88e7f14ad4b27247cbf786c73fae5ef9f4ae2166
Omar Ong <marionong@gmail.com>
2021-11-15 00:22:15 -0500

    contact page wip

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,17 +2,56 @@ import React, {useEffect, useState} from 'react';
 import sanityClient from "../client";
 import {Breadcrumb, BreadcrumbItem, Button, Card, Col, Container, Image, Row, Stack} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
-import PortableText from "@sanity/block-content-to-react";
+import smileVirtual from '../assets/images/smileVirtualMobile.png';
 import styled from "styled-components";
 import TestimonialSection from "./TestimonialSection";
 import PreFooter from "./PreFooter";
 import Footer from "./Footer";
-
+import bull from '../assets/images/bull.png';
 
 const Div85WidthCentered = styled.div`
   width: 85%;
   margin: 0 auto;
   align-items: center;
+`
+
+const StyledConsultImage = styled(Image)`
+  padding-right: 75px;
+  @media (max-width: 768px) {
+    padding-right: 15px;
+  }
+
+  
+`
+
+const StyledImg = styled(Image)`
+  border-radius: 5px;
+`
+
+const StyledText = styled.p`
+    //font-size: 20px;
+  
+    @media (min-width: 769px) {
+      font-size: 20px;
+    }
+
+`
+
+const DaysText = styled.p`
+    padding-right: 65px;
+    @media (min-width: 769px) {
+      font-size: 20px;
+    }
+
+`
+
+const RightCol = styled(Col)`
+  padding: 0 90px;
+  
+  @media (max-width: 768px) {
+    padding: 0 12px;
+    
+  }
 `
 
 function PatientInfo(props) {
@@ -60,7 +99,7 @@ function PatientInfo(props) {
                                 </div>
 
 
-                                <Breadcrumb>
+                                <Breadcrumb className="mt-4">
                                     <LinkContainer to="/">
                                         <BreadcrumbItem>
                                           <a href="home">
@@ -82,12 +121,11 @@ function PatientInfo(props) {
                                     </h1>
                                 </div>
                                 <Div85WidthCentered>
-                                    <h3 className="text-center">
+                                    <h3 className="text-center my-md-5">
                                         We would love to hear from you! Please choose the method of communication most convenient to you.
                                         {/*<PortableText blocks={content.subHeadline}/>*/}
                                     </h3>
                                 </Div85WidthCentered>
-
 
                         </span>
                     )
@@ -96,12 +134,85 @@ function PatientInfo(props) {
                 })}
 
 
-                <TestimonialSection/>
-                <PreFooter/>
-                <Footer/>
+                    <Row>
+                        <Col md={5}>
+                            <StyledImg
+                                src={bull}
+                                alt="wall street bull"
+                                fluid
+                            />
+                        </Col>
+                        <RightCol md={7} className=" py-3 py-md-0">
+                            <StyledText>
+                                <strong>Visit us at our convenient location in the heart of the Financial District.</strong>
+                                </StyledText>
+                            <StyledText className="pt-md-4">
 
+                                <strong>Our Address</strong> <br/>
+                                39 Broadway Suite 2115 <br/>
+                                New York, NY 10006
+                            </StyledText>
+                            <StyledText>
+                                <strong>Give Us a Call</strong> <br/>
+                                Tel: 212-422-9229 <br/>
+                                Fax: 212-742-0928
+                            </StyledText>
+                            <StyledText>
+                                <strong>Send Us An Email</strong> <br/>
+                                Email: reception@theexchangedentalgroup.com
+                            </StyledText>
+                        </RightCol>
+                    </Row>
+
+                    <Row className="mt-3 mt-md-5">
+                        <Col md={5}>
+                            <Stack gap={3}>
+                                <StyledText>
+                                    <strong>You may also reach us with these online contact options:</strong>
+                                </StyledText>
+                                <a href="https://app.smilevirtual.com/Dr-Todd-Goldstein/sign-up" target="_blank">
+                                    <StyledConsultImage src={smileVirtual} fluid />
+                                </a>
+                            </Stack>
+
+
+                        </Col>
+                        <RightCol md={7} className=" py-3 py-md-0">
+                            <StyledText>
+                                <strong>Our Office Hours</strong>
+                            </StyledText>
+                            <div className="d-flex flex-nowrap">
+                                <DaysText className="pt-md-4">
+                                    Monday <br/>
+                                    Tuesday <br/>
+                                    Wednesday <br/>
+                                    Thursday <br/>
+                                    Friday <br/>
+                                    Saturday <br/>
+                                    Sunday
+                                </DaysText>
+                                <StyledText className="pt-md-4">
+                                    7:45 am - 7:00 pm <br/>
+                                    7:45 am - 7:00 pm <br/>
+                                    7:45 am - 7:00 pm <br/>
+                                    7:45 am - 7:00 pm <br/>
+                                    7:45 am - 4:00 pm <br/>
+                                    Closed <br/>
+                                    7:45 am - 7:00 pm <br/>
+                                </StyledText>
+                            </div>
+
+                        </RightCol>
+                    </Row>
+
+                    <TestimonialSection/>
+                    <PreFooter/>
+                    <Footer/>
                 </Stack>
             </Container>
+
+
+
         </>
     );
 }
