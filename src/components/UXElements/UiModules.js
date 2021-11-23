f9c57b8d3a3fbc4f7354d62b7cb67b23191d1ac4
Omar Ong <marionong@gmail.com>
2021-11-23 16:27:25 -0500

    new component module exports

---
+++ src/components/UXElements/UiModules.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import {LinkContainer} from "react-router-bootstrap";
+
+export const StyledLinkContainer = styled(LinkContainer)`
+  cursor: pointer;
+  &:hover {
+    color: #6da4fd;
+  }
+`
