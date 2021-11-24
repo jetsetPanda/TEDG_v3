6ac90600a5f01795163ead9aff4ed8053a3b37c9
Omar Ong <marionong@gmail.com>
2021-11-24 18:56:33 -0500

    ui modules

--- src/components/UXElements/UiModules.js
+++ src/components/UXElements/UiModules.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import {LinkContainer} from "react-router-bootstrap";
+import {CardImgOverlay} from "reactstrap";
 
 export const StyledLinkContainer = styled(LinkContainer)`
   cursor: pointer;
@@ -11,4 +12,48 @@ export const StyledLinkContainer = styled(LinkContainer)`
 export const SubHeaderCopy = styled.h5`
   font-size: 20px !important;
   text-align: center;
+`
+
+export const HoverOverlay = styled(CardImgOverlay)`
+    border-radius: 15px;
+    background: rgba(0,0,0,0.7);
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    opacity: 0;
+    -webkit-transition: all 0.4s ease-in-out 0s;
+    -moz-transition: all 0.4s ease-in-out 0s;
+    transition: all 0.4s ease-in-out 0s;
+
+    &:hover {
+      opacity: 1;
+    }
+`
+
+export const HoverOverlayContent = styled.div`
+  position: absolute;
+  text-align: center;
+  padding-left: 1em;
+  padding-right: 1em;
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  opacity: 0;
+  -webkit-transform: translate(-50%, -50%);
+  -moz-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  -webkit-transition: all 0.3s ease-in-out 0s;
+  -moz-transition: all 0.3s ease-in-out 0s;
+  transition: all 0.3s ease-in-out 0s;
+  
+  &:hover {
+    top: 50%;
+    left: 50%;
+    opacity: 1;
+  }
+
 `
