fb080491f518ebad6414ded0deadb28d1c93c854
Omar Ong <marionong@gmail.com>
2021-11-24 19:17:30 -0500

    sorting schema

--- tedgv3/schemas/servicesList.js
+++ tedgv3/schemas/servicesList.js
@@ -4,6 +4,12 @@ export default {
     type: "document",
     fields: [
         {
+            name: "order",
+            title: "Order",
+            type: "number",
+            hidden: true,
+        },
+        {
             name: "serviceName",
             title: "Service Name",
             type: "string",
